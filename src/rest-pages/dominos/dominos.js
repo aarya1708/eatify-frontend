80804f27baa77f52cbe196de12541520c65db030
aarya1708 <javiaaarya2003@gmail.com>
2025-06-24 17:56:50 +0530

    Update dominos.js

--- src/rest-pages/dominos/dominos.js
+++ src/rest-pages/dominos/dominos.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import Header from '../../components/header/header2.js';
 import '../pizzahut/pizzahut.css';
 import { Menuitem } from '../menu_card.js';
